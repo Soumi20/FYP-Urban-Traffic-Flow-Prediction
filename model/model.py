303da4a5619f79abad7d7c8aac32c001fcc23efc
Soumi20 <48052638+Soumi20@users.noreply.github.com>
2021-03-27 20:52:19 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -25,29 +25,6 @@ def get_lstm(units):
     return model
 
 
-# def get_gru(units):
-#     """GRU(Gated Recurrent Unit)
-#     Build GRU Model.
-
-#     # Arguments
-#         units: List(int), number of input, output and hidden units.
-#     # Returns
-#         model: Model, nn model.
-#     """
-
-#     model = Sequential()
-
-#     # model.add(GRU(units[1], input_shape=(units[0], 1), return_sequences=True))
-#     model.add(GRU(units[1], input_shape=(units[0], 1), return_sequences=True, reset_after=True))
-
-    
-#     # model.add(GRU(units[1], input_shape=(units[0], 1), return_sequences=True))
-#     model.add(GRU(units[2]))
-#     model.add(Dropout(0.2))
-#     model.add(Dense(units[3], activation='sigmoid'))
-
-#     return model
-
 
 def _get_sae(inputs, hidden, output):
     """SAE(Auto-Encoders)
